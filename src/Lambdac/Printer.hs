a4aa582b72b47b362a47e6449eaa2a5697c456ae
vglfr <vf.velt@gmail.com>
2021-12-02 17:53:18 +0200

    WIP tree display (Nix migration)

--- src/Lambdac/Printer.hs
+++ src/Lambdac/Printer.hs
@@ -4,8 +4,6 @@ module Lambdac.Printer where
 
 import Lambdac.Syntax
 
-import Control.Monad (join)
-
 instance Show Expr where
   show (Var x)   = x
   show (Abs h b) = "λ" ++ show h ++ showAbs b
@@ -19,14 +17,14 @@ instance Show Expr where
 
 class Repr a where
   repr :: a -> String
-  tree :: a -> String
+  -- tree :: a -> String
 
 instance Repr Expr where
   repr (Var x)   = "(Var \"" ++ x ++ "\")"
   repr (Abs h b) = "(Abs " ++ repr h ++ " " ++ repr b ++ ")"
   repr (App f x) = "(App " ++ repr f ++ " " ++ repr x ++ ")"
-  tree (Var x)   = x
-  tree (Abs h b) = undefined
+  -- tree (Var x)   = x
+  -- tree (Abs h b) = undefined
 
 {-
 
@@ -44,187 +42,103 @@ instance Repr Expr where
     ├ λ z z
     └ y
 
-Bad:
-
-       ∙
-      / \
-     /   \
-    /     \
-   /       \
-  λ         λ
- / \       / \
-x   ∙     y   ∙
-   / \       / \
-  x   λ     λ   y
-     / \   / \
-    z   z z   z
-
-[
-  [0]
-  [-2, 2]
-  [-4, 0, 0, 4]
-  [-2, 2, 2, 6]
-  [ 0, 4, 6, 10] +6 for the RIGHTMOST branch
-]
-
-[
-  [3]
-  [-2, 8]
-  [-4, 0, 6, 10]
-  [-2, 2, 8, 12]
-  [ 0, 4, 6, 10]
-]
-
-  ┌ x
-┌ λ 
-│ │ ┌ x
-│ └ ∙
-│   └ λ z z
-∙ 
-│ ┌ y
-└ λ
-  │ ┌ λ z z
-  └ ∙
-    └ y
-
-∙
-├ λ
-│ ├ x
-│ └ ∙
-│   ├ x
-│   └ λ
-│     ├ z
-│     └ z
-└ λ
-  ├ y
-  └ ∙
-    ├ λ
-    │ ├ z
-    │ └ z
-    └ y
-
-∙
-├─ λ
-│  ├─ x
-│  └─ ∙
-│     ├─ x
-│     └─ λ
-│        ├─ z
-│        └─ z
-└─ λ
-   ├─ y
-   └─ ∙
-      ├─ λ
-      │  ├─ z
-      │  └─ z
-      └─ y
-
-  ┌ x
-┌ λ 
-│ │ ┌ x
-│ └ ∙
-│   │ ┌ z
-│   └ λ
-∙     └ z
-│ ┌ y
-└ λ   ┌ z
-  │ ┌ λ
-  │ │ └ z
-  └ ∙
-    └ y
-
  -}
 
-data Slash = S Int | B Int
+--           ├        │         -- └ on pop from Glyphs
+data Glyph = VR Int | VV Int deriving Show
+type Glyphs = [Glyph]
 
-instance Show Slash where
-  show (S _) = "\ESC[30m/\ESC[m"
-  show (B _) = "\ESC[30m\\\ESC[m"
+expr' :: Expr
+expr' = λ "x" (λ "y" "x")
+-- expr' = λ "x" "x"
+-- expr' = "x"
+-- expr' = λ "x" ("x" ⋅ λ "z" "z") ⋅ λ "y" (λ "z" "z" ⋅ "y")
 
-type Elements   = [[String]]
-type Offsets    = [[Int]]
-type Slashes    = [[Slash]]
+type Frame = (Expr, Int)
+type Stack = [Frame]
+
+tree :: Expr -> String
+-- tree = unlines . go [] . pure . (,0)
+-- tree e = unlines . go [] $ [(e,0)]
+tree e = go [] [(e,0)] []
+ where
+  go :: [String] -> Stack -> Glyphs -> String
+  go ls [] _  = unlines . reverse $ ls
+  go ls fs gs = let (l,fs',gs') = line fs gs
+                 in go (l : ls) fs' gs'
+
+line :: Stack -> Glyphs -> (String, Stack, Glyphs)
+line (a:as) gs = let (s,as',gs') = f a gs
+                  in (s, as' ++ as, gs')
+ where
+  f :: Frame -> Glyphs -> (String, [Frame], Glyphs)
+  f (e,o) gs = if simp e
+                  then (skeleton ur o ++ leaf e, [], gs) -- └
+                  else (skeleton vr o ++ root e, map (\x -> (x,o+2)) (leaves e), gs ++ [VR o]) -- ├
+
+skeleton :: String -> Int -> String
+skeleton cs n =
+  if n == 0
+     then replicate n ' '
+     else replicate (n-2) ' ' ++ cs
+
+vv :: String
+vv = "│ "
+
+vr :: String
+vr = "├ "
+
+ur :: String
+ur = "└ "
+
+sh :: String
+sh = "\ESC[30m"
+
+eh :: String
+eh = "\ESC[m"
+
+offset :: Glyph -> Int
+offset (VR n) = n
+offset (VV n) = n
+
+glyphs' :: Glyphs
+-- glyphs' = [VR 0]
+-- glyphs' = [VV 0, VR 2]
+glyphs' = [VV 0, VV 4, VV 8, VR 10]
+
+glyphs :: Glyphs -> Int -> String
+glyphs (g:gs) o = concat $ drawHead g : map drawTail (zip gs (g:gs)) ++ [offset']
+ where
+  drawHead :: Glyph -> String
+  drawHead (VR n) = replicate n ' ' ++ vr
+  drawHead (VV n) = replicate n ' ' ++ vv
+
+  drawTail :: (Glyph, Glyph) -> String
+  drawTail (VR e, s) = replicate (e - offset s - 2) ' ' ++ vr
+  drawTail (VV e, s) = replicate (e - offset s - 2) ' ' ++ vv
+
+  offset' :: String
+  offset' = if null gs
+               then ""
+               else replicate (o - offset (last gs) - 2) ' '
+
+simp :: Expr -> Bool
+simp (Var _) = True
+simp (Abs (Var _) (Var _)) = True
+simp (App (Var _) (Var _)) = True
+simp _ = False
+
+leaf :: Expr -> String
+leaf (Var x) = x
+leaf (Abs h b) = unwords ["λ", show h, show b]
+leaf (App f x) = unwords [show f, "∙", show x]
 
 root :: Expr -> String
 root (Var x)   = x
 root (Abs _ _) = "λ"
-root (App _ _) = "⋅"
+root (App _ _) = "∙"
 
 leaves :: Expr -> [Expr]
 leaves (Var x)   = []
 leaves (Abs h b) = [h, b]
 leaves (App f x) = [f, x]
-
-expr' :: Expr
--- expr' = λ "x" (λ "y" "x")
--- expr' = λ "x" "x"
-expr' = λ "x" ("x" ⋅ λ "z" "z") ⋅ λ "y" (λ "z" "z" ⋅ "y")
-
-elements :: Expr -> Elements
-elements e = go [] [e]
- where
-  go acc [] = acc
-  go acc es = go (acc ++ [map root es]) (concatMap leaves es)
-
-offsets :: Expr -> Offsets
-offsets e = go [] [(e,0)]
- where
-  go acc [] = acc
-  go acc es = go (acc ++ [map snd es]) (concatMap leaves' es)
-
-  leaves' (Var _, n) = []
-  leaves' (    e, n) = let ls = leaves e
-                        in [(head ls, n-2), (last ls, n+2)]
-
-balance :: Offsets -> Offsets
-balance os = let ls = last os
-                 ds = diff ls
-              in os
-
-diff :: [Int] -> [Int]
-diff xs = 0 : map (\(e,s) -> e - s) (zip (tail xs) xs)
-
--- slashes :: Expr -> Slashes
--- slashes = undefined
-
-elements' :: Elements
-elements' =
-  [ ["∙"]
-  , ["λ", "λ"]
-  , ["x", "∙", "y", "∙"]
-  , ["x", "λ", "λ", "y"]
-  , ["z", "z", "z", "z"]
-  ]
-
-offsets' :: Offsets
-offsets' =
-  [ [3]
-  , [-2, 8]
-  , [-4, 0, 6, 10]
-  , [-2, 2, 8, 12]
-  , [ 0, 4, 6, 10]
-  ]
-
-slashes' :: Slashes
-slashes' =
-  [ [S 0]
-  , [S (-3), B (-1), S 7, B 9]
-  , [S (-1), B   1,  S 9, B 11]
-  , [S   1,  B   3,  S 7, B 9]
-  , [S 0]
-  ]
-
-draw :: Elements -> Offsets -> Slashes -> IO ()
-draw es os ss = mapM_ f (zip3 es os ss)
- where
-  f (es',os',ss') = do
-    let o  = head os'
-        ds = map (\(e,s) -> e - s - 1) $ zip (tail os') os'
-        s  = replicate (5 + head os') ' ' ++ head es' ++ concatMap offset (zip (tail es') ds)
-        so  = map (\s -> case s of (S n) -> n; (B n) -> n) ss'
-        ds' = map (\(e,s) -> e - s - 1) $ zip (tail so) so
-        s'  = replicate (5 + head so) ' ' ++ show (head ss') ++ concatMap offsetS (zip (tail ss') ds')
-    putStrLn s
-    putStrLn s'
-  offset  (e,o) = replicate o ' ' ++ e
-  offsetS (s,o) = replicate o ' ' ++ show s
